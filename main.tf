a6f7cb6403e9a7bc9e4f9f78a688098ea543897f
Nitin Gullah <nitin.sc.gullah@gmail.com>
2021-02-17 19:19:17 -0500

    Add a .gitignore file

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "example" {
+  ami = "ami-047a51fa27710816e"
+  instance_type = "t2.micro"
+  tags = {
+    "Name" = "terraform-example"
+  }
+}
